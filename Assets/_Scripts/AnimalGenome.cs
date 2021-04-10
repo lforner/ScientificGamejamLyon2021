c32609a235348a3b8e07f32f196e37eec62240f6
Nico04 <34476051+Nico04@users.noreply.github.com>
2021-04-10 21:09:31 +0200

    Basic moves

--- Assets/_Scripts/AnimalGenome.cs
+++ Assets/_Scripts/AnimalGenome.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 
 public class AnimalGenome {
     public float Speed = 1;
-    public float Fertility = 1;
+
+    /// <summary>
+    /// Fertility, from 0 to 0.5
+    /// </summary>
+    public float Fertility = 0.25f;
     public float Visibility = 1;
     public float EnergyEfficiency = 1;
+
+    /*AnimalGenome ChildGenome(AnimalGenome parent) {
+        return null;
+    }*/
 }
